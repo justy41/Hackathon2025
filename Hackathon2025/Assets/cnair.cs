c7206ddf3507cfdf88fa07690c490a58064bc2de
Tudor Voie <tudor_voie@yahoo.com>
2025-04-05 11:34:02 +0300

    iustine te rog fa line renderer pupatias talpile

--- Hackathon2025/Assets/cnair.cs
+++ Hackathon2025/Assets/cnair.cs
@@ -1,6 +1,12 @@
 using System;
+using System.Collections.Generic;
 using System.Drawing;
+using System.Linq;
+using System.Numerics;
+using System.Transactions;
+using Unity.Collections;
 using Unity.VisualScripting;
+using UnityEditor.SearchService;
 using UnityEngine;
 using UnityEngine.U2D;
 
@@ -8,18 +14,34 @@ public class cnair : MonoBehaviour
 {
     // Start is called once before the first execution of Update after the MonoBehaviour is created 
     
-    Transform transform;
-
+    [SerializeField] LineRenderer lineRenderer;
+    private UnityEngine.Vector2 mp;
+    private bool start = false;
     void Start()
     {
+        lineRenderer.useWorldSpace = true;
     }
+
+    public List<UnityEngine.Vector3> pozitii = new List<UnityEngine.Vector3>();
+    int cnt = 0;
 
     // Update is called once per frame
     void Update()
     {
-        if(Input.GetMouseButton(0)) {
-            if(transform == null) Debug.Log("caca");
+        if(Input.GetMouseButtonDown(0)) {
+            if(start == false) {
+                start = true;
+                mp = Input.mousePosition;
+                UnityEngine.Vector3 sp = Camera.main.ScreenToWorldPoint(mp);
+                sp.z = 0f;
+                pozitii.Add(sp);
+            } else {
+                mp = Input.mousePosition;
+                UnityEngine.Vector3 sp = Camera.main.ScreenToWorldPoint(mp);
+                sp.z = 0f;
+                pozitii.Add(sp);
+                
+            }
         }
     }
-
 }
