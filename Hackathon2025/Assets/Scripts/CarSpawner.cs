38ac55bc77b7c85f7dfb40461dcc628d9a67f639
Babiciu Iustin <167506994+justy41@users.noreply.github.com>
2025-04-05 14:31:57 +0300

    nigg

--- Hackathon2025/Assets/Scripts/CarSpawner.cs
+++ Hackathon2025/Assets/Scripts/CarSpawner.cs
@@ -1,20 +1,27 @@
+using System.Collections.Generic;
 using UnityEngine;
 
 public class CarSpawner : MonoBehaviour
 {
-    public float spawnTimer;
-    
-    public Transform[][] routes;
+    public GameObject car;
+    float spawnTimer;
+    public float spawnTimerValue;
+    public Transform[] points;
     
     // Start is called once before the first execution of Update after the MonoBehaviour is created
     void Start()
     {
-        
+        spawnTimer = spawnTimerValue;
+        car.GetComponent<FollowPoints>().points = points;
     }
 
     // Update is called once per frame
     void Update()
     {
-        
+        spawnTimer -= Time.deltaTime;
+        if(spawnTimer <= 0) {
+            Instantiate(car, transform.position, Quaternion.identity);
+            spawnTimer = spawnTimerValue;
+        }
     }
 }
