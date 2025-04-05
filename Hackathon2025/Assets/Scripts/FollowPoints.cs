7668ca31584b5d42c55d8b430419ea16389c13c1
Babiciu Iustin <167506994+justy41@users.noreply.github.com>
2025-04-05 11:40:22 +0300

    added priority to the cars

--- Hackathon2025/Assets/Scripts/FollowPoints.cs
+++ Hackathon2025/Assets/Scripts/FollowPoints.cs
@@ -1,3 +1,4 @@
+using System;
 using UnityEngine;
 
 public class FollowPoints : MonoBehaviour
@@ -6,6 +7,8 @@ public class FollowPoints : MonoBehaviour
     public Transform[] points;
     Transform currentPoint;
     int currentIndex = 0;
+    
+    bool isTouchingCar;
     
     // Start is called once before the first execution of Update after the MonoBehaviour is created
     void Start()
@@ -16,13 +19,34 @@ public class FollowPoints : MonoBehaviour
     // Update is called once per frame
     void Update()
     {
+        
         if(transform.transform.position != points[points.Length - 1].position) {
             if(Vector2.Distance(transform.position, currentPoint.position) <= 0) {
                 currentIndex++;
                 currentPoint = points[currentIndex];
             }
             
-            this.transform.position = Vector2.MoveTowards(transform.position, currentPoint.position, speed * Time.deltaTime);
+            transform.rotation = Quaternion.Euler(0, 0, Mathf.Atan((currentPoint.position.y-transform.position.y)/(currentPoint.position.x-transform.position.x)) * Mathf.Rad2Deg);
+            
+            if(!isTouchingCar) {
+                this.transform.position = Vector2.MoveTowards(transform.position, currentPoint.position, speed * Time.deltaTime);
+            }
+        }
+    }
+
+    void OnTriggerEnter2D(Collider2D other)
+    {
+        Debug.Log("fkhjkfj");
+        if(other.CompareTag("car")) {
+            isTouchingCar = true;
+            Debug.Log("am atins masina");
+        }
+    }
+
+    void OnTriggerExit2D(Collider2D other)
+    {
+        if(other.CompareTag("car")) {
+            isTouchingCar = false;
         }
     }
 }
